ea646942e2270024aad3beccb92a829edd48485a
EtshD1 <mohamed.h.abouelenin@gmail.com>
2022-04-10 23:54:14 +0200

    include posts route

--- src/Routes/index.ts
+++ src/Routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import users from "./users";
+import posts from "./posts";
 
 const router = Router();
 
 router.use(users);
+router.use(posts);
 
 export default router;
