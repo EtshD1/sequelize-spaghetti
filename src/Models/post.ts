8a8b9d027c17ad7a7dbf48952d7bbb80714b3cb1
EtshD1 <mohamed.h.abouelenin@gmail.com>
2022-04-10 22:43:52 +0200

    post modal

---
+++ src/Models/post.ts
@@ -0,0 +1,34 @@
+import sequelize from ".";
+import { Model, DataTypes } from "sequelize";
+
+class Post extends Model {
+	static associate() {}
+
+	toJSON() {
+		return { ...this.get() };
+	}
+}
+
+Post.init(
+	{
+		uuid: {
+			type: DataTypes.UUID,
+			defaultValue: DataTypes.UUIDV4,
+		},
+		body: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				notNull: { msg: "Post must have a body" },
+				notEmpty: { msg: "Body must not be empty" },
+			},
+		},
+	},
+	{
+		sequelize,
+		modelName: "Post",
+		tableName: "posts",
+	}
+);
+
+export default Post;
