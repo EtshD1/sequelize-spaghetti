59ee19d479ad4cf95342e22cf8496611bb664a24
EtshD1 <mohamed.h.abouelenin@gmail.com>
2022-04-10 23:35:48 +0200

    Include user routes to API

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import db from "./Models";
 import User from "./Models/user";
 import Post from "./Models/post";
 import express from "express";
+import router from "./Routes";
 
 User.hasMany(Post, { foreignKey: "owner", as: "posts" });
 Post.belongsTo(User, { foreignKey: "owner", as: "user" });
 
 const app = express();
 app.use(express.json());
+
+app.use(router);
 
 app.listen(8080, async () => {
 	console.log("Syncing Database");
